fac2b3c0c080ece980a8b2b0d88a1d6aa147783a
takiya <b2282@oic.jp>
2015-02-26 19:40:49 +0900

    list表示途中

--- todolist/todo.js
+++ todolist/todo.js
@@ -1,17 +1,28 @@
 $(loaded);
 
-function loaded(){
+function loaded() {
+ todolist();
  //ボタンを押されたときに実行
  $("#button").click(
   function(){
  	 saveText();
+ 	 todolist();
  });
 }
 
-function saveText(){
+function saveText() {
  var text = $("#Text");
  var time = new Date();
  localStorage.setItem(time, text.val());
 
- text.val();
+ text.val("");
+}
+
+function todolist() {
+ for(var i = 0; i < localStorage.length; i++){
+  var key = localStorage.key(i);
+  var value = localStorage.getItem(key)
+		//cosole.log(localStorage.getItem(key));
+  html.push("<p>" + value + "</p>");
+	}
 }
