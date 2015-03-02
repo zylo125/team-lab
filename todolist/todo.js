052e9ada9269c98fea96d1044e7b3170eeb5a9a2
takiya <b2282@oic.jp>
2015-03-02 13:15:14 +0900

    更新

--- todolist/todo.js
+++ todolist/todo.js
@@ -13,6 +13,8 @@ function loaded() {
 function localsave() {
  var text = $("#Text");
  var time = new Date();
+ new Date( JSON.parse( JSON.stringify(time) ) );
+ console.log(time);
  localStorage.setItem(time, text.val());
 
  text.val(""); //textboxを空にする
@@ -27,7 +29,12 @@ function todolist() {
  for(var i=0; i<localStorage.length; i++){
   key = localStorage.key(i);
   value = localStorage.getItem(key);
-  html.push("<p>"  + value +"</p>");
+ // var length = localStorage.length;
+ // length--;
+ // for(var i=length; 0 < i; i--){
+ //  key = localStorage.key(i);
+ //  value = localStorage.getItem(key)
+  html.push("<li>"  + value +"</li>");
  }
  list.append(html.join(''));
 }
